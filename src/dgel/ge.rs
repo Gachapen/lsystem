ec5fb02836ea0be426c20be2923c947a3ddb986b
Magnus Bjerke Vik <mbvett@gmail.com>
2017-09-22 16:55:59 +0200

    [dgel-ge] Modify TournamentSelector to avoid unnecessary cloning

--- src/dgel/ge.rs
+++ src/dgel/ge.rs
@@ -14,7 +14,7 @@ use rand::distributions::{IndependentSample, Range};
 use serde_yaml;
 use rsgenetic::pheno::{Fitness, Phenotype};
 use rsgenetic::sim::{Builder, NanoSecond, RunResult, SimResult, Simulation, StepResult};
-use rsgenetic::sim::select::{MaximizeSelector, Selector, TournamentSelector};
+use rsgenetic::sim::select::{MaximizeSelector, Selector, Parents};
 use futures::future::{self, Future};
 use futures_cpupool::CpuPool;
 use num_cpus;
@@ -818,5 +818,60 @@ where
 {
     fn build(self) -> Simulator<'a, T, F> {
         self.sim
+    }
+}
+
+/// Runs several tournaments, and selects best performing phenotypes from each tournament.
+#[derive(Copy, Clone, Debug)]
+pub struct TournamentSelector {
+    count: usize,
+    participants: usize,
+}
+
+impl TournamentSelector {
+    /// Create and return a tournament selector.
+    ///
+    /// Such a selector runs `count / 2` tournaments, each with `participants` participants.
+    /// From each tournament, the best 2 phenotypes are selected, yielding
+    /// `count` parents.
+    ///
+    /// * `count`: must be larger than zero, a multiple of two and less than the population size.
+    /// * `participants`: must be larger than zero and less than the population size.
+    pub fn new(count: usize, participants: usize) -> TournamentSelector {
+        TournamentSelector {
+            count: count,
+            participants: participants,
+        }
+    }
+}
+
+impl<T, F> Selector<T, F> for TournamentSelector
+    where T: Phenotype<F>,
+          F: Fitness
+{
+    fn select(&self, population: &[T]) -> Result<Parents<T>, String> {
+        if self.count == 0 || self.count % 2 != 0 || self.count * 2 >= population.len() {
+            return Err(format!("Invalid parameter `count`: {}. Should be larger than zero, a \
+                                multiple of two and less than half the population size.",
+                               self.count));
+        }
+        if self.participants == 0 || self.participants >= population.len() {
+            return Err(format!("Invalid parameter `participants`: {}. Should be larger than \
+                                zero and less than the population size.",
+                               self.participants));
+        }
+
+        let mut result: Parents<T> = Vec::new();
+        let mut rng = ::rand::thread_rng();
+        for _ in 0..(self.count / 2) {
+            let mut tournament: Vec<&T> = Vec::with_capacity(self.participants);
+            for _ in 0..self.participants {
+                let index = rng.gen_range::<usize>(0, population.len());
+                tournament.push(&population[index]);
+            }
+            tournament.sort_by(|x, y| y.fitness().cmp(&x.fitness()));
+            result.push((tournament[0].clone(), tournament[1].clone()));
+        }
+        Ok(result)
     }
 }
