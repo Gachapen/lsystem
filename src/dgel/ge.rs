4306ac95eaf252ef54de7579c1281425b411784f
Magnus Bjerke Vik <mbvett@gmail.com>
2017-09-22 18:47:21 +0200

    [dgel-ge] Use custom fork of rsgenetic

--- src/dgel/ge.rs
+++ src/dgel/ge.rs
@@ -14,7 +14,7 @@ use rand::distributions::{IndependentSample, Range};
 use serde_yaml;
 use rsgenetic::pheno::{Fitness, Phenotype};
 use rsgenetic::sim::{Builder, NanoSecond, RunResult, SimResult, Simulation, StepResult};
-use rsgenetic::sim::select::{MaximizeSelector, Selector, Parents};
+use rsgenetic::sim::select::{MaximizeSelector, Selector, TournamentSelector};
 use futures::future::{self, Future};
 use futures_cpupool::CpuPool;
 use num_cpus;
@@ -670,28 +670,40 @@ where
             return StepResult::Done;
         }
 
-        // Perform selection
-        let parents = match self.selector.select(self.population) {
-            Ok(parents) => parents,
-            Err(e) => {
-                println!("Error selecting parents: {}", e);
-                return StepResult::Failure;
-            }
-        };
+        let mut children: Vec<T> = {
+            // Perform selection
+            let parents = match self.selector.select(self.population) {
+                Ok(parents) => parents,
+                Err(e) => {
+                    println!("Error selecting parents: {}", e);
+                    return StepResult::Failure;
+                }
+            };
 
-        let mut children: Vec<T> = if self.do_crossover {
-            parents
-                .iter()
-                .map(|&(ref a, ref b)| a.crossover(b))
-                .collect()
-        } else {
-            let (parents_a, parents_b): (Vec<T>, Vec<T>) = parents.into_iter().unzip();
-            parents_a.into_iter().chain(parents_b.into_iter()).collect()
-        };
+            let num_children = parents.len() / 2;
+            let children: Vec<T> = if self.do_crossover && self.mutate {
+                parents
+                    .iter()
+                    .take(num_children)
+                    .zip(parents.iter().skip(num_children))
+                    .map(|(a, b)| a.crossover(b))
+                    .map(|c| c.mutate())
+                    .collect()
+            } else if self.do_crossover {
+                parents
+                    .iter()
+                    .take(num_children)
+                    .zip(parents.iter().skip(num_children))
+                    .map(|(a, b)| a.crossover(b))
+                    .collect()
+            } else if self.mutate {
+                parents.into_iter().take(num_children).map(|c| c.mutate()).collect()
+            } else {
+                parents.into_iter().take(num_children).cloned().collect()
+            };
 
-        if self.mutate {
-            children = children.iter().map(|c| c.mutate()).collect();
-        }
+            children
+        };
 
         // Kill off parts of the population at random to make room for the children
         self.kill_off(children.len());
@@ -818,60 +830,5 @@ where
 {
     fn build(self) -> Simulator<'a, T, F> {
         self.sim
-    }
-}
-
-/// Runs several tournaments, and selects best performing phenotypes from each tournament.
-#[derive(Copy, Clone, Debug)]
-pub struct TournamentSelector {
-    count: usize,
-    participants: usize,
-}
-
-impl TournamentSelector {
-    /// Create and return a tournament selector.
-    ///
-    /// Such a selector runs `count / 2` tournaments, each with `participants` participants.
-    /// From each tournament, the best 2 phenotypes are selected, yielding
-    /// `count` parents.
-    ///
-    /// * `count`: must be larger than zero, a multiple of two and less than the population size.
-    /// * `participants`: must be larger than zero and less than the population size.
-    pub fn new(count: usize, participants: usize) -> TournamentSelector {
-        TournamentSelector {
-            count: count,
-            participants: participants,
-        }
-    }
-}
-
-impl<T, F> Selector<T, F> for TournamentSelector
-    where T: Phenotype<F>,
-          F: Fitness
-{
-    fn select(&self, population: &[T]) -> Result<Parents<T>, String> {
-        if self.count == 0 || self.count % 2 != 0 || self.count * 2 >= population.len() {
-            return Err(format!("Invalid parameter `count`: {}. Should be larger than zero, a \
-                                multiple of two and less than half the population size.",
-                               self.count));
-        }
-        if self.participants == 0 || self.participants >= population.len() {
-            return Err(format!("Invalid parameter `participants`: {}. Should be larger than \
-                                zero and less than the population size.",
-                               self.participants));
-        }
-
-        let mut result: Parents<T> = Vec::new();
-        let mut rng = ::rand::thread_rng();
-        for _ in 0..(self.count / 2) {
-            let mut tournament: Vec<&T> = Vec::with_capacity(self.participants);
-            for _ in 0..self.participants {
-                let index = rng.gen_range::<usize>(0, population.len());
-                tournament.push(&population[index]);
-            }
-            tournament.sort_by(|x, y| y.fitness().cmp(&x.fitness()));
-            result.push((tournament[0].clone(), tournament[1].clone()));
-        }
-        Ok(result)
     }
 }
